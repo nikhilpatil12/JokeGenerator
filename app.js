060ce6712394f56676589966e5ae396938f96d83
nikhilpatil12 <patil.nikhil12@outlook.com>
2024-01-05 18:58:16 -0800

    changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createCanvas, loadImage } = require("canvas");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const fs = require("fs");
 app.use(express.static("public"));
 
