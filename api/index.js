36c1cfe98d28465869aa740a754c9048456eedc4
nikhilpatil12 <patil.nikhil12@outlook.com>
2024-01-05 16:31:00 -0800

    fixes

--- api/index.js
+++ api/index.js
@@ -5,42 +5,48 @@ const fs = require("fs");
 
 app.get("/api/image", async (req, res) => {
   try {
-    jokenumber = getRandomInt(104);
-    // jokenumber = 29;
+    fetch("./jokes.json")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        jokes = json;
+        jokenumber = getRandomInt(104);
+        // jokenumber = 29;
 
-    const jokesData = fs.readFileSync("jokes.json", "utf-8");
-    const jokes = JSON.parse(jokesData);
-    // console.log(jokes.size())
-    var canvas = createCanvas(450, 150);
-    const ctx = canvas.getContext("2d");
+        // const jokesData = fs.readFileSync("jokes.json", "utf-8");
+        // const jokes = JSON.parse(jokesData);
+        // console.log(jokes.size())
+        var canvas = createCanvas(450, 150);
+        const ctx = canvas.getContext("2d");
 
-    // ctx.fillStyle = "#242423";
-    // ctx.fillRect(0, 0, 300, 200);
-    let grd = ctx.createLinearGradient(0, 0, 450, 150);
-    grd.addColorStop(0, "#00a0ff");
-    grd.addColorStop(1, "#12cba6");
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, 450, 150);
+        // ctx.fillStyle = "#242423";
+        // ctx.fillRect(0, 0, 300, 200);
+        let grd = ctx.createLinearGradient(0, 0, 450, 150);
+        grd.addColorStop(0, "#00a0ff");
+        grd.addColorStop(1, "#12cba6");
+        ctx.fillStyle = grd;
+        ctx.fillRect(0, 0, 450, 150);
 
-    ctx.strokeStyle = "#fff";
-    ctx.rect(10, 10, 430, 130);
-    ctx.stroke();
+        ctx.strokeStyle = "#fff";
+        ctx.rect(10, 10, 430, 130);
+        ctx.stroke();
 
-    ctx.fillStyle = "#ffffff";
-    ctx.font = "15px Arial";
+        ctx.fillStyle = "#ffffff";
+        ctx.font = "15px Arial";
 
-    ctx.fillText("Q: " + jokes.jokes[jokenumber].question, 20, 50);
-    ctx.fillText("A: " + jokes.jokes[jokenumber].answer, 20, 100);
+        ctx.fillText("Q: " + jokes.jokes[jokenumber].question, 20, 50);
+        ctx.fillText("A: " + jokes.jokes[jokenumber].answer, 20, 100);
 
-    // var image = new Image();
-    // image.id = "pic";
-    // image.src = canvas.toDataURL();
-    // // document.appendChild(image);
+        // var image = new Image();
+        // image.id = "pic";
+        // image.src = canvas.toDataURL();
+        // // document.appendChild(image);
 
-    const dataUrl = canvas.toDataURL("image/png");
+        const dataUrl = canvas.toDataURL("image/png");
 
-    // Send the data URL as a response
-    res.send(`<img src="${dataUrl}" alt="Generated Joke"/>`);
+        // Send the data URL as a response
+        res.send(`<img src="${dataUrl}" alt="Generated Joke"/>`);
+      });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
